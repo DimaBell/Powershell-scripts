1443747ebfa4c1b6a77b37b4d74e6f353c5d55f6
DimaBell <96667530+DimaBell@users.noreply.github.com>
2022-08-23 17:43:17 +0300

    Update OpenConnections.ps1

--- OpenConnections.ps1
+++ OpenConnections.ps1
@@ -1,11 +1,12 @@
 <#
+    A powershell script that outputs information of all the running processes that have established connections
     
+    including the remote IP address and remote port to which each process is connected to.
     
     Script writer: Dima Bell.
    
     E-mail: D1m419935@gmail.com 
 #>
-
 
 $OpenConnections = Get-NetTCPConnection -State Established | Where-Object { ($_.RemoteAddress -ne "127.0.0.1") -and ($_.RemoteAddress -ne "::1") } | Select-Object RemoteAddress, RemotePort, OwningProcess
 Foreach ($OpenConnection in $OpenConnections) {
